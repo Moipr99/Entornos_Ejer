c63b5270f36b214e4d2e9d51133d7710fb7dc762
Moipr99 <moipr9@gmail.com>
2026-02-12 13:22:32 +0100

    Subida carpeta del proyecto

---
+++ PROYECTO_DAM_0.1.0/src/Classes/Config.java
@@ -0,0 +1,11 @@
+package Classes; // V0.2.0
+
+public class Config {
+	public static final String VERSION = "0.2.0";
+	public static final String WELCOME = "Bienvenido al desafio del laberinto";
+	public static final String GOODBYE = "¡Hasta la proxima!";
+	public static final String UNLOGGED_MENU = "MENU\n--------\n1-Iniciar sesion\n2-Registro\n0-Salir";
+	public static final String LOGGED_MENU = "MENU\n-------\n1-Cargar laberinto\n2-Ver laberinto actual\n3-Establecer casillas de entrada y salida\n4-Buscar caminos\n5-Ver usuario actual\n6-Cerrar sesion\n0-Salir";
+		
+}
+
